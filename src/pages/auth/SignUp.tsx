cd90763655245ae476c426188aa40307998b62fa
unsnruu <unsnruu@gmail.com>
2022-08-13 18:38:09 +0900

    feat: create submit event handler

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigato = useNavigate();
 
   const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.currentTarget.value);
@@ -10,10 +13,21 @@ function SignUp() {
   const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.currentTarget.value);
   };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    resetValues();
+  };
+  const resetValues = () => {
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div>
       <h1>Sign Up</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">이메일</label>
           <input
