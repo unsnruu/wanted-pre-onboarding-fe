6d51c1b4a0997dc60903e341c733b991dfb40ac5
unsnruu <unsnruu@gmail.com>
2022-08-13 17:06:54 +0900

    feat: configure axios and emotion setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
+import { Global, css } from "@emotion/react";
+
+axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.withCredentials = true;
+
+const globalStyles = css`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Global styles={globalStyles} />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
