f46f6a2fc0f9d1f28b37a953800e138ff0152dc7
unsnruu <unsnruu@gmail.com>
2022-08-13 19:54:52 +0900

    feat: clone from SignUp compo

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,7 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [disabled, setDisabled] = useState(true);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const validateEmail = (email: string) => {
+      if (email.includes("@")) return true;
+      else return false;
+    };
+    const validatePassword = (password: string) => {
+      if (password.length >= 8) return true;
+      else return false;
+    };
+
+    if (validateEmail(email) && validatePassword(password)) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [email, password]);
+
+  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.currentTarget.value);
+  };
+  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    resetValues();
+  };
+  const resetValues = () => {
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div>
-      <h1>Sign In</h1>
+      <h1>로그인</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">이메일</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="이메일을 입력해주세요"
+            value={email}
+            onChange={handleChangeEmail}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">비밀번호</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handleChangePassword}
+          />
+        </div>
+        <button disabled={disabled}>가입하기</button>
+      </form>
     </div>
   );
 }
