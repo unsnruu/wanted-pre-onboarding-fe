b1eaae6612894c69ba8913066e40496b5fe4dd60
unsnruu <unsnruu@gmail.com>
2022-08-13 18:33:07 +0900

    feat: implement form

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,7 +1,40 @@
+import React, { useState } from "react";
+
 function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.currentTarget.value);
+  };
+  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value);
+  };
   return (
     <div>
       <h1>Sign Up</h1>
+      <form>
+        <div>
+          <label htmlFor="email">이메일</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="이메일을 입력해주세요"
+            value={email}
+            onChange={handleChangeEmail}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">비밀번호</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handleChangePassword}
+          />
+        </div>
+        <button>가입하기</button>
+      </form>
     </div>
   );
 }
