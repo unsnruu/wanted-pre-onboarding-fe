ea638cbd7440dfdce9725a0a0ef0201e6881eab8
unsnruu <unsnruu@gmail.com>
2022-08-13 17:30:24 +0900

    feat: add a new route

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import axios from "axios";
 import { Global, css } from "@emotion/react";
+
+import App from "./pages/App";
+import NotFound from "./pages/404";
 
 axios.defaults.baseURL = "http://localhost:8000";
 axios.defaults.withCredentials = true;
 
 const globalStyles = css`
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui,
+      Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo",
+      "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Symbol", sans-serif;
+  }
 `;
 
 const root = ReactDOM.createRoot(
@@ -21,7 +29,10 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Global styles={globalStyles} />
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
